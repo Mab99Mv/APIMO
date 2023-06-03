54aa9c574a2cb587f48d0caeba9d2fcd1b1b5c3f
Mab99V <mabelperezgaribay@gmail.com>
2023-06-03 12:28:40 -0500

    cambios

--- routes/routes.js
+++ routes/routes.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const controllerReactions = require('../controllers/ContollerSumaryReactions');
-const controllerComments = require('../controllers/ControllerComments')
+const controllerComments = require('../controllers/ControllerComments');
 
 router.get('/reactions/:objectId/:reactionId', controllerReactions.getDocument);
+router.post('/reactions', controllerReactions.createDocument);
 router.get('/comments/:objectId', controllerComments.getDocumentsByObjectId);
 
 module.exports = router;
-
-
 
