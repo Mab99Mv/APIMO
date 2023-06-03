b57f530be390c9102a080e168a1c8434a78b1cfb
Mab99V <mabelperezgaribay@gmail.com>
2023-06-03 12:16:33 -0500

    cambios

--- controllers/ContollerSumaryReactions.js
+++ controllers/ContollerSumaryReactions.js
@@ -21,3 +21,34 @@ exports.getDocument = async (req, res) => {
     res.status(500).send('Error interno del servidor');
   }
 };
+
+exports.createDocument = async (req, res) => {
+  const { objectId, reactionId } = req.body;
+
+  try {
+    const existingDocument = await MyModel.findOne({
+      '_id.objectId': objectId,
+      '_id.reactionId': reactionId,
+    });
+
+    if (existingDocument) {
+      console.log('El documento ya existe:', existingDocument);
+      res.status(400).send('El documento ya existe');
+    } else {
+      const newDocument = new MyModel({
+        _id: {
+          objectId: objectId,
+          reactionId: reactionId,
+        },
+      });
+
+      await newDocument.save();
+      console.log('Documento creado:', newDocument);
+      res.status(201).json(newDocument);
+    }
+  } catch (error) {
+    console.error('Error al crear el documento:', error);
+    res.status(500).send('Error interno del servidor');
+  }
+};
+
