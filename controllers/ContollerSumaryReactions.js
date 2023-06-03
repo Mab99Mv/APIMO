1b8588575e0ee4ba18b65a9bedb9d9f8c36beb24
Mab99V <mabelperezgaribay@gmail.com>
2023-06-03 17:01:53 -0500

    cambios

--- controllers/ContollerSumaryReactions.js
+++ controllers/ContollerSumaryReactions.js
@@ -1,11 +1,10 @@
-const MyModel = require('../models/ModelSumaryReactions');
-
 exports.getDocument = async (req, res) => {
   const { objectId, reactionId } = req.params;
 
   try {
     const documento = await MyModel.findOne({
-      _id: { objectId: objectId, reactionId: reactionId },
+      objectId: objectId,
+      reactionId: reactionId,
     });
 
     if (documento) {
@@ -17,35 +16,6 @@ exports.getDocument = async (req, res) => {
     }
   } catch (error) {
     console.error('Error en la consulta:', error);
-    res.status(500).send('Error interno del servidor');
-  }
-};
-
-exports.createDocument = async (req, res) => {
-  const { objectId, reactionId } = req.body;
-
-  try {
-    const existingDocument = await MyModel.findOne({
-      _id: { objectId: objectId, reactionId: reactionId },
-    });
-
-    if (existingDocument) {
-      console.log('El documento ya existe:', existingDocument);
-      res.status(400).send('El documento ya existe');
-    } else {
-      const newDocument = new MyModel({
-        _id: {
-          objectId: objectId,
-          reactionId: reactionId,
-        },
-      });
-
-      await newDocument.save();
-      console.log('Documento creado:', newDocument);
-      res.status(201).json(newDocument);
-    }
-  } catch (error) {
-    console.error('Error al crear el documento:', error);
     res.status(500).send('Error interno del servidor');
   }
 };
