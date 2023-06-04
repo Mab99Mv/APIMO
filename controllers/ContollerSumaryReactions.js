abbb797b51cd62d3ab1ae8d3a41a1d84e7453db4
Mab99Mv <106404376+Mab99Mv@users.noreply.github.com>
2023-06-03 18:23:32 -0600

    Update ContollerSumaryReactions.js

--- controllers/ContollerSumaryReactions.js
+++ controllers/ContollerSumaryReactions.js
@@ -1,3 +1,6 @@
+const MyModel = require('../models/MyModel');
+
+// Obtener un documento por objectId y reactionId
 exports.getDocument = async (req, res) => {
   const { objectId, reactionId } = req.params;
 
@@ -16,6 +19,29 @@ exports.getDocument = async (req, res) => {
     }
   } catch (error) {
     console.error('Error en la consulta:', error);
+    res.status(500).send('Error interno del servidor');
+  }
+};
+
+// Crear un nuevo documento
+exports.createDocument = async (req, res) => {
+  try {
+    const { objectId, reactionId, data } = req.body;
+
+    // Crea un nuevo documento utilizando el modelo correspondiente
+    const nuevoDocumento = new MyModel({
+      objectId: objectId,
+      reactionId: reactionId,
+      data: data
+    });
+
+    // Guarda el documento en la base de datos
+    const documentoCreado = await nuevoDocumento.save();
+
+    console.log('Documento creado:', documentoCreado);
+    res.json(documentoCreado);
+  } catch (error) {
+    console.error('Error al crear el documento:', error);
     res.status(500).send('Error interno del servidor');
   }
 };
