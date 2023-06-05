13d8b650cc0e939e4f906ca20a00bf63b599db5a
Mab99Mv <106404376+Mab99Mv@users.noreply.github.com>
2023-06-05 12:24:22 -0600

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const controllerReactions = require('../controllers/ContollerSumaryReactions');
 const controllerComments = require('../controllers/ControllerComments');
 
-router.get('/api/reactions/:objectId/count', controllerReactions.getDocumentReactionsCount);
-router.get('/api/reactions', controllerReactions.getAllDocuments);
-router.post('/api/reactions', controllerReactions.createDocument);
-router.get('/api/comments/:objectId', controllerComments.getDocumentsByObjectId);
+router.get('/reactions/:objectId/count', controllerReactions.getDocumentReactionsCount);
+router.get('/reactions', controllerReactions.getAllDocuments);
+router.post('/reactions', controllerReactions.createDocument);
+router.get('/comments/:objectId', controllerComments.getDocumentsByObjectId);
 
 module.exports = router;
 
